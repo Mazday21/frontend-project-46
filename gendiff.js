2942081a14b61d7e864abf73aee0a73d7745fbc6
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-21 14:17:21 +0300

    add --format

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-program.version("1.0.0");
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version("1.0.0")
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    // Здесь будет логика сравнения файлов и вывода разницы
+    // Вы можете использовать options.format для доступа к выбранному формату
+    console.log(`Comparing ${filepath1} and ${filepath2} in format ${options.format}`);
+  });
+
 program.parse(process.argv);
