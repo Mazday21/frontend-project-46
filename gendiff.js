d77750f2a22f8eecb42b20c1783d2685b612429c
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-21 17:19:12 +0300

    add parse.js 3 step

--- gendiff.js
+++ gendiff.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import parsingFile from './src/parseFromFile.js';
 
 program
-  .version("1.0.0")
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
-    // Здесь будет логика сравнения файлов и вывода разницы
-    // Вы можете использовать options.format для доступа к выбранному формату
-    console.log(`Comparing ${filepath1} and ${filepath2} in format ${options.format}`);
+    const arrStrs = parsingFile(filepath1, filepath2, options.format);
+    console.log(arrStrs[0], arrStrs[1]);
   });
 
 program.parse(process.argv);
