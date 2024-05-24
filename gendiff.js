eff9927037bc89fbb3bde0d5de7e65b8eb6e718d
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-24 16:12:59 +0300

    compare + str-obj

--- gendiff.js
+++ gendiff.js
@@ -2,14 +2,39 @@
 import { program } from 'commander';
 import parsingFile from './src/parseFromFile.js';
 
+function compare(obj1, obj2) {
+  const result = {};
+  const keys = new Set([...Object.keys(obj1), ...Object.keys(obj2)]);
+  const sortedKeys = Array.from(keys).sort();
+
+  sortedKeys.forEach((key) => {
+    const hasKey1 = Object.prototype.hasOwnProperty.call(obj1, key);
+    const hasKey2 = Object.prototype.hasOwnProperty.call(obj2, key);
+
+    if (hasKey1 && !hasKey2) {
+      result[`- ${key}`] = obj1[key];
+    } else if (!hasKey1 && hasKey2) {
+      result[`+ ${key}`] = obj2[key];
+    } else if (hasKey1 && hasKey2 && obj1[key] !== obj2[key]) {
+      result[`- ${key}`] = obj1[key];
+      result[`+ ${key}`] = obj2[key];
+    } else if (hasKey1 && hasKey2) {
+      result[`  ${key}`] = obj1[key];
+    }
+  });
+
+  return result;
+}
+
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
-    const arrStrs = parsingFile(filepath1, filepath2, options.format);
-    console.log(arrStrs[0], arrStrs[1]);
+    const arrObjs = parsingFile(filepath1, filepath2, options.format);
+    const differences = compare(arrObjs[0], arrObjs[1]);
+    console.log(JSON.stringify(differences));
   });
 
 program.parse(process.argv);
--- src/parseFromFile.js
+++ src/parseFromFile.js
@@ -4,7 +4,7 @@ import path from 'path';
 export default function parseFile(filePath1, filePath2) {
   const getFileFormat = (filePath) => path.extname(filePath);
   if (getFileFormat(filePath1) !== '.json' || getFileFormat(filePath2) !== '.json') throw new Error(`Error unknown file format from ${filePath1}, ${filePath2}`);
-  const data1 = readFileSync(filePath1, 'utf8');
-  const data2 = readFileSync(filePath2, 'utf8');
+  const data1 = JSON.parse(readFileSync(filePath1, 'utf8'));
+  const data2 = JSON.parse(readFileSync(filePath2, 'utf8'));
   return [data1, data2];
 }
