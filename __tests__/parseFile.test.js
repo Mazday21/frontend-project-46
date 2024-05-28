abf0331aca929bea5ddf825c3b02a1631cad25c2
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-28 10:35:32 +0300

    -mock files(TypeError: readFileSync.mockImplementation)

--- __tests__/parseFile.test.js
+++ __tests__/parseFile.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { jest } from '@jest/globals';
-import { readFileSync } from 'node:fs';
+// import { jest } from '@jest/globals';
+// import { readFileSync } from 'node:fs';
 import path from 'path';
 import parseFile from '../src/parseFromFile.js';
 
-jest.mock('node:fs');
+// jest.mock('node:fs');
 
 const filepath1 = path.resolve('file1.json');
 const filepath2 = path.resolve('file2.json');
@@ -22,20 +22,20 @@ const obj2 = {
   host: 'hexlet.io',
 };
 
-readFileSync.mockImplementation((filepath) => {
-  if (filepath === filepath1) {
-    return JSON.stringify(obj1);
-  }
-  if (filepath === filepath2) {
-    return JSON.stringify(obj2);
-  }
-  return null;
-});
+// readFileSync.mockImplementation((filepath) => {
+//   if (filepath === filepath1) {
+//     return JSON.stringify(obj1);
+//   }
+//   if (filepath === filepath2) {
+//     return JSON.stringify(obj2);
+//   }
+//   return null;
+// });
 
 test('parseFile', () => {
   expect(parseFile(filepath1, filepath2)).toEqual([obj1, obj2]);
 });
 
-afterAll(() => {
-  jest.restoreAllMocks();
-});
+// afterAll(() => {
+//   jest.restoreAllMocks();
+// });
