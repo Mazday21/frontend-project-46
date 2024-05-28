154ed677915ee4560799fed55db35ecb2e069f50
”Dmitry <vvvvvvmaz@gmail.com>
2024-05-28 11:47:23 +0300

    fix

--- __tests__/parseFile.test.js
+++ __tests__/parseFile.test.js
@@ -1,13 +1,8 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// import { jest } from '@jest/globals';
-// import { readFileSync } from 'node:fs';
 import path from 'path';
 import parseFile from '../src/parseFromFile.js';
 
-// jest.mock('node:fs');
-
-const filepath1 = path.resolve('file1.json');
-const filepath2 = path.resolve('file2.json');
+const filepath1 = path.resolve('__fixtures__/file1.json');
+const filepath2 = path.resolve('__fixtures__/file2.json');
 
 const obj1 = {
   host: 'hexlet.io',
@@ -22,20 +17,6 @@ const obj2 = {
   host: 'hexlet.io',
 };
 
-// readFileSync.mockImplementation((filepath) => {
-//   if (filepath === filepath1) {
-//     return JSON.stringify(obj1);
-//   }
-//   if (filepath === filepath2) {
-//     return JSON.stringify(obj2);
-//   }
-//   return null;
-// });
-
 test('parseFile', () => {
   expect(parseFile(filepath1, filepath2)).toEqual([obj1, obj2]);
 });
-
-// afterAll(() => {
-//   jest.restoreAllMocks();
-// });
