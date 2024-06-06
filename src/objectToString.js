aeb6efd5ac18863411b10393570c499c3fb2c7f2
”Dmitry <vvvvvvmaz@gmail.com>
2024-06-06 10:56:36 +0300

    close indents

--- src/objectToString.js
+++ src/objectToString.js
@@ -1,19 +1,24 @@
-export default function objectToString(obj, indent = '') {
+export default function objectToString(obj, indent = '', closeIndent = '') {
   const lines = ['{'];
   const entries = Object.entries(obj);
 
   entries.forEach(([key, value], index) => {
     if (typeof value === 'object' && value !== null) {
-      const nestedIndent = `${indent}    `;
-      const nestedLines = objectToString(value, nestedIndent);
+      let nestedIndent = `${indent}    `;
+      if (key.startsWith('+') || key.startsWith('-')) {
+        nestedIndent = `${indent}      `;
+      }
+      const nestedCloseIndent = `${closeIndent}    `;
+      const nestedLines = objectToString(value, nestedIndent, nestedCloseIndent);
       lines.push(`${indent}  ${key}: ${nestedLines}`);
     } else {
       lines.push(`${indent}  ${key}: ${value}`);
     }
 
     if (index === entries.length - 1) {
-      lines.push(`${indent}}`);
+      lines.push(`${closeIndent}}`);
     }
   });
+
   return lines.join('\n');
 }
