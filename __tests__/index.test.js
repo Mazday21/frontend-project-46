5a837da601b3e80d5ebe7385607baaa474b3942a
”Dmitry <vvvvvvmaz@gmail.com>
2024-06-07 14:14:50 +0300

    code climate fixes

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,34 +10,28 @@ const absolutePath = (filePath) => {
 
 const readFile = (filename) => fs.readFileSync(absolutePath(filename), 'utf-8');
 
-test('JSON to str formatted', () => {
-  const file1 = absolutePath('./__fixtures__/file1.json');
-  const file2 = absolutePath('./__fixtures__/file2.json');
-  const expected = readFile('./__fixtures__/result.txt');
+const file1JSON = absolutePath('./__fixtures__/file1.json');
+const file2JSON = absolutePath('./__fixtures__/file2.json');
+const file1YML = absolutePath('./__fixtures__/file1.yml');
+const file2YML = absolutePath('./__fixtures__/file2.yml');
+const resultStylish = readFile('./__fixtures__/result.txt');
 
-  expect(gendiff(file1, file2)).toEqual(expected);
+test('JSON to str formatted', () => {
+  expect(gendiff(file1JSON, file2JSON)).toEqual(resultStylish);
 });
 
 test('YML to str formatted', () => {
-  const file1 = absolutePath('./__fixtures__/file1.yml');
-  const file2 = absolutePath('./__fixtures__/file2.yml');
-  const expected = readFile('./__fixtures__/result.txt');
-
-  expect(gendiff(file1, file2)).toEqual(expected);
+  expect(gendiff(file1YML, file2YML)).toEqual(resultStylish);
 });
 
 test('plain', () => {
-  const file1 = absolutePath('./__fixtures__/file1.json');
-  const file2 = absolutePath('./__fixtures__/file2.json');
-  const expected = readFile('./__fixtures__/resultPlain.txt');
+  const resultPlain = readFile('./__fixtures__/resultPlain.txt');
 
-  expect(gendiff(file1, file2, 'plain')).toEqual(expected);
+  expect(gendiff(file1JSON, file2JSON, 'plain')).toEqual(resultPlain);
 });
 
 test('JSON', () => {
-  const file1 = absolutePath('./__fixtures__/file1.json');
-  const file2 = absolutePath('./__fixtures__/file2.json');
-  const expected = readFile('./__fixtures__/resultJSON.txt');
+  const resultJSON = readFile('./__fixtures__/resultJSON.txt');
 
-  expect(gendiff(file1, file2, 'json')).toEqual(expected);
+  expect(gendiff(file1JSON, file2JSON, 'json')).toEqual(resultJSON);
 });
--- src/compareObjects.js
+++ src/compareObjects.js
@@ -2,32 +2,29 @@ import _ from 'lodash';
 
 const diff = (obj1, obj2) => {
   const sortedUniKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-  const resultObj = sortedUniKeys.map((key) => {
+
+  return sortedUniKeys.reduce((acc, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
+    let item = { key };
 
     if (!Object.hasOwn(obj1, key)) {
-      return { key, value: value2, type: 'added' };
+      item = { ...item, value: value2, type: 'added' };
+    } else if (!Object.hasOwn(obj2, key)) {
+      item = { ...item, value: value1, type: 'deleted' };
+    } else if (value1 === value2) {
+      item = { ...item, value: value1, type: 'unchanged' };
+    } else if (typeof value1 === 'object' && typeof value2 === 'object' && value1 !== null && value2 !== null) {
+      item = { ...item, value: diff(value1, value2), type: 'hasChild' };
+    } else {
+      item = {
+        ...item, oldValue: value1, value: value2, type: 'changed',
+      };
     }
 
-    if (!Object.hasOwn(obj2, key)) {
-      return { key, value: value1, type: 'deleted' };
-    }
-
-    if (value1 === value2) {
-      return { key, value: value1, type: 'unchanged' };
-    }
-
-    if (typeof value1 === 'object' && typeof value2 === 'object') {
-      return { key, value: diff(value1, value2), type: 'hasChild' };
-    }
-    return {
-      key,
-      oldValue: value1,
-      value: value2,
-      type: 'changed',
-    };
-  });
-  return resultObj;
+    acc.push(item);
+    return acc;
+  }, []);
 };
+
 export default diff;
