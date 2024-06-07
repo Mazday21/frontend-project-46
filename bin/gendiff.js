2ea9b3e04a6e4dc371bc78734d5bb0f0a1ecb8d5
”Dmitry <vvvvvvmaz@gmail.com>
2024-06-07 20:51:37 +0300

    hex

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ import genDiff from '../src/index.js';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2, options) => {
     const diff = genDiff(filepath1, filepath2, options.format);
